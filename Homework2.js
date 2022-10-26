186906526da61ee9d1c6415ac93d43c4abc30be7
malekmkaddem <79175484+malekmkaddem@users.noreply.github.com>
2022-10-25 18:46:42 -0700

    Update Homework2.js

--- Homework2.js
+++ Homework2.js
@@ -1,7 +1,4 @@
-/* Homework2 Part 1*/
-document.write( "<strong style='color:red;font-family: Times New Roman,sans-serif;'>xy </strong>");
-document.write( "<strong style='color:blue;font-family:Arial,sans-serif;'>12 </strong>");
-document.write( "<strong style='color:green;font-family:Impact,sans-serif;'>89cc</strong>");
+
 
 /* Homework2 Part 2*/
 const $ = selector => document.querySelector(selector);
